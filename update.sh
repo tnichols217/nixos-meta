d33df3dc1d695d970fec0e5398424161729ee2c1
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-11 10:44:15 +0800

    finish update scripts

--- update.sh
+++ update.sh
@@ -1,10 +1,22 @@
 git submodule init
-git submodule update
-cd nixos-config
+
+cd nixos-program-extensions
 git checkout main
 git pull
 sh ./update.sh
-cd ../nixos-program-extensions
+git add -A
+git commit -am "automated update at $(date)"
+git push
+
+cd ../nixos-config
 git checkout main
 git pull
 sh ./update.sh
+git add -A
+git commit -am "automated update at $(date)"
+git push
+
+cd ..
+git add -A
+git commit -am "automated update at $(date)"
+git push
