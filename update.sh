c868fcee8b36d029777aeba7ee95cdde7d34e4af
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-11 10:56:56 +0800

    add shell

--- update.sh
+++ update.sh
@@ -4,19 +4,19 @@ cd nixos-program-extensions
 git checkout main
 git pull
 sh ./update.sh
-git add -A
-git commit -am "automated update at $(date)"
-git push
+# git add -A
+# git commit -am "automated update at $(date)"
+# git push
 
 cd ../nixos-config
 git checkout main
 git pull
 sh ./update.sh
-git add -A
-git commit -am "automated update at $(date)"
-git push
+# git add -A
+# git commit -am "automated update at $(date)"
+# git push
 
 cd ..
-git add -A
-git commit -am "automated update at $(date)"
-git push
+# git add -A
+# git commit -am "automated update at $(date)"
+# git push
